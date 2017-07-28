4808b3fbc68fe3ee94149c88d28173f63266696e
NathPaiva <hello@nathpaiva.com.br>
2017-07-28 10:55:02 -0300

    fix user name

--- public/src/components/header/Header.js
+++ public/src/components/header/Header.js
@@ -2,55 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../../images/logo.png';
+import style from './header.css';
 
-const style = {
-  header: {
-    width: '100%',
-    margin: '0 auto',
-    borderBottom: '1px solid rgb(90, 74, 105)',
-    backgroundColor: '#fff'
-  },
-  container: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    maxWidth: '500px',
-    margin: 'auto'
-  },
-  containerLogout: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    maxWidth: '500px',
-    margin: 'auto'
-  },
-  utils: {
-    width: '150px',
-    justifyContent: 'space-between',
-    display: 'flex',
-    fontSize: '12px',
-  },
-  img: {
-    width: '90px'
-  },
-  link: {
-    textDecoration: 'underline',
-  },
-  hide: {
-    display: 'block',
-    position: 'absolute',
-    textIndent: '-9999px'
-  },
+
+const user = () => {
+  return JSON.parse(localStorage.getItem('user'))
 };
-
-const user = JSON.parse(localStorage.getItem('user'));
 
 const Header = () => (
   <header style={style.header}>
     <div style={!!localStorage.getItem('user') ? style.container : style.containerLogout}>
       <h1><img style={style.img} src={logo} alt='Caffeine' /><span style={style.hide}>Caffeine</span></h1>
-      {!!localStorage.getItem('user') && <div style={style.utils}>
-        Welcome {user.user_name}
+      {!!user() && <div style={style.utils}>
+        <span style={style.welcomeblock}>Welcome {user().user_name}</span>
         <Link style={style.link} to='/logout'>logout</Link>
       </div>}
     </div>
---
+++ public/src/components/header/header.css.js
@@ -0,0 +1,47 @@
+const style = {
+  header: {
+    width: '100%',
+    margin: '0 auto',
+    borderBottom: '1px solid rgb(90, 74, 105)',
+    backgroundColor: '#fff'
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    maxWidth: '500px',
+    margin: 'auto'
+  },
+  containerLogout: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    maxWidth: '500px',
+    margin: 'auto'
+  },
+  utils: {
+    width: '150px',
+    justifyContent: 'space-between',
+    display: 'flex',
+    fontSize: '12px',
+    alignItems: 'center',
+  },
+  welcomeblock: {
+    maxWidth: '90px',
+    display: 'block',
+    wordWrap: 'break-word',
+  },
+  img: {
+    width: '90px'
+  },
+  link: {
+    textDecoration: 'underline',
+  },
+  hide: {
+    display: 'block',
+    position: 'absolute',
+    textIndent: '-9999px'
+  },
+};
+
+export default style;
