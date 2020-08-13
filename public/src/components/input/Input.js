ec4a52a38f5a6e42ce129a6f8fff20ca68da19b7
Nath Paiva <hello@nathpaiva.com.br>
2020-08-13 09:40:37 -0300

    Change Input component

--- public/src/components/input/Input.js
+++ public/src/components/input/Input.js
@@ -1,49 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const style = {
-  styleInputDisabled: {
-    border: '1px solid #CCCCCC',
-    padding: '0 5px',
-    margin: '2px 0',
-    width: '100%',
-    height: '32px',
-    opacity: '.5',
-  },
-  styleInput: {
-    border: '1px solid #CCCCCC',
-    padding: '0 5px',
-    margin: '2px 0',
-    width: '100%',
-    borderRadius: '2px',
-    height: '32px',
-  },
-  styleLabel: {
-    fontSice: '12px',
-    fontFamily: '"Roboto", sans-serif',
-  },
-};
+const Root = styled.input`
+  border: 1px solid #CCCCCC;
+  padding: 0 5px;
+  margin: 2px 0;
+  width: 100%;
+  border-radius: 2px;
+  height: 32px;
 
-const Input = ({ type, id, text, label, disabled, inputRef, defaultValue }) => {
+  &:disabled {
+    opacity: .5;
+  }
+`;
+
+const Input = ({ type, id, placeholder, disabled, inputRef }) => {
   return (
-    <div>
-      {label ? (
-        <label style={style.styleLabel} htmlFor={id}>
-          {label}
-        </label>
-      ) : (
-        ''
-      )}
-      <input
-        ref={inputRef}
-        style={!disabled ? style.styleInput : style.styleInputDisabled}
-        defaultValue={defaultValue ? defaultValue : ''}
-        disabled={disabled ? disabled : false}
-        type={type}
-        id={id}
-        name={id}
-        placeholder={text}
-      />
-    </div>
+    <Root
+      ref={inputRef}
+      disabled={disabled}
+      type={type}
+      id={id}
+      name={id}
+      placeholder={placeholder}
+    />
   );
 };
 
