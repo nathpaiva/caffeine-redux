a4459c9fab50479b4f5f21500152d936c2e2ae7e
Nath Paiva <hello@nathpaiva.com.br>
2020-08-13 09:40:19 -0300

    Change Button component

--- public/src/components/button/Button.js
+++ public/src/components/button/Button.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
-var buttonStyle = {
-  fontSize: '12px',
-  padding: '10px',
-  textAlign: 'center',
-  textTransform: 'uppercase',
-  cursor: 'pointer',
-  backgroundColor: '#5A4A69',
-  color: '#FFFFFF',
-  fontWeight: 'bold',
-  width: '100%',
-  border: 'none',
-  borderRadius: '2px',
-};
+const Root = styled.button`
+  font-size: 1.2rem;
+  padding: 10px;
+  text-align: center;
+  text-transform: uppercase;
+  cursor: pointer;
+  background-color: #5A4A69;
+  color: #FFFFFF;
+  font-weight: bold;
+  width: 100%;
+  border: none;
+  border-radius: 2px;
+`;
 
-
-const Button = ({ name }) => (
-  <button style={buttonStyle}>{name}</button>
+const Button = ({ children, ...rest }) => (
+  <Root {...rest}>{children}</Root>
 );
 
 export default Button;
